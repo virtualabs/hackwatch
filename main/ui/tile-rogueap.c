65a3001a76f3775725170ed8401cebf0efc4db32
Damien Cauquil <virtualabs@gmail.com>
2021-06-18 00:30:41 +0200

    Improved buttons (size and color) and eco mode is disabled on this tile.

--- main/ui/tile-rogueap.c
+++ main/ui/tile-rogueap.c
@@ -8,6 +8,7 @@ widget_button_t select_btn, startstop_btn;
 modal_t ap_select_modal;
 wifiscan_t ap_wifiscan;
 widget_button_t ap_select_ok_btn;
+widget_frame_t ap_frame;
 wifi_ap_t *target_ap = NULL;
 char psz_target_ap[64];
 char psz_target_channel[64];
@@ -21,6 +22,9 @@ int rogueap_tile_event_handler(tile_t *p_tile, tile_event_t event, int x, int y,
       {
         /* Enable scanner. */
         wifi_set_mode(WIFI_SCANNER);
+
+        /* Disable eco mode. */
+        disable_ecomode();
       }
       break;
 
@@ -28,6 +32,9 @@ int rogueap_tile_event_handler(tile_t *p_tile, tile_event_t event, int x, int y,
       {
         /* Stop scanner. */
         wifi_set_mode(WIFI_OFF);
+
+        /* Re-enable eco mode. */
+        enable_ecomode();
       }
       break;
 
@@ -101,49 +108,59 @@ tile_t *tile_rogueap_init(void)
   widget_label_init(&title_lbl, &rogueap_tile, 3, 3, 200, 40, "Rogue AP");
   widget_set_front_color(&title_lbl, RGB(0x0, 0x8, 0xc));
 
+  /* Add frame. */
+  widget_frame_init(&ap_frame, &rogueap_tile, 2, 39, 240-2, 240-39-2);
+  widget_set_border_color((widget_t *)&ap_frame, RGB(0x0, 0x8, 0xc));
+
   /* No AP selected. */
   psz_target_ap[0] = '\0';
   strncpy(psz_target_channel, "Channel: -", 64);
-  widget_label_init(&target_lbl, &rogueap_tile, 3, 46, 200, 40, "Target:");
-  widget_label_init(&essid_lbl, &rogueap_tile, 3, 90, 200, 40, psz_target_ap);
-  widget_label_init(&channel_lbl, &rogueap_tile, 3, 130, 200, 40, psz_target_channel);
+  widget_label_init(&target_lbl, &rogueap_tile, 7, 46, 200, 40, "Target:");
+  widget_label_init(&essid_lbl, &rogueap_tile, 7, 90, 200, 40, psz_target_ap);
+  widget_label_init(&channel_lbl, &rogueap_tile, 7, 130, 200, 40, psz_target_channel);
 
   /* Add our buttons. */
   widget_button_init(
     &select_btn,
     &rogueap_tile,
-    5,
-    185,
-    (240-15)/2,
+    7,
+    180,
+    110,
     50,
     "Sel. AP"
   );
+  widget_set_border_color((widget_t *)&select_btn, RGB(0x0, 0x8, 0xc));
+  widget_set_bg_color((widget_t *)&select_btn, RGB(0x0, 0x8, 0xc));
   widget_button_set_handler(&select_btn, select_btn_onclick);
 
   widget_button_init(
     &startstop_btn,
     &rogueap_tile,
     10 + (240-15)/2,
-    185,
+    180,
     (240-15)/2,
     50,
     "Start"
   );
+  widget_set_border_color((widget_t *)&startstop_btn, RGB(0x0, 0x8, 0xc));
+  widget_set_bg_color((widget_t *)&startstop_btn, RGB(0x0, 0x8, 0xc));
   widget_button_set_handler(&startstop_btn, start_btn_onclick);
 
   /* Initialize our select AP dialog box. */
-  modal_init(&ap_select_modal, 0, 0, 230, 230);
+  modal_init(&ap_select_modal, 0, 0, 240, 238);
   widget_button_init(
     &ap_select_ok_btn,
     &ap_select_modal.tile,
-    65,
-    175,
+    60,
+    180,
     120,
     50, 
     "OK"
   );
+  widget_set_border_color((widget_t *)&ap_select_ok_btn, RGB(0x0, 0x8, 0xc));
+  widget_set_bg_color((widget_t *)&ap_select_ok_btn, RGB(0x0, 0x8, 0xc));
   widget_button_set_handler(&ap_select_ok_btn, ap_select_modal_onclick);
-  wifiscan_init(&ap_wifiscan, &ap_select_modal.tile, 5, 5, 220, 168);
+  wifiscan_init(&ap_wifiscan, &ap_select_modal.tile, 5, 5, 230, 168);
 
   /* Return our tile. */
   return &rogueap_tile;
