3d840b6cda3c9967b30ce78bd435b9391a2325ba
Damien Cauquil <virtualabs@gmail.com>
2022-04-01 00:40:26 +0200

    Added BLE icons (used by the BLE scanner).

---
+++ main/img/ble_icons.h
@@ -0,0 +1,9 @@
+#ifndef __INC_BLE_ICONS_H
+#define __INC_BLE_ICONS_H
+#include "twatch.h"
+
+__attribute__ ((aligned(4)))
+extern const uint16_t ble_icons[];
+
+#endif /* __INC_BLE_ICONS_H */
+
---
+++ main/img/ble_icons_big.h
@@ -0,0 +1,9 @@
+#ifndef __INC_BLE_ICONS_BIG_H
+#define __INC_BLE_ICONS_BIG_H
+#include "twatch.h"
+
+__attribute__ ((aligned(4)))
+extern const uint16_t ble_icons_big[];
+
+#endif /* __INC_BLE_ICONS_BIG_H */
+
